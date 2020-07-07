f6d79de3386d0360007de6cdfc408365ffd622c0
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-07-07 17:59:33 +0900

    [finish]java_base_chapter4

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "Neko";
+		System.out.println(str1);
+		str1 = "Cat";
+		System.out.println(str1);
+
+		final int number;
+		number = 7;
+		System.out.println(number);
+	}
+}
