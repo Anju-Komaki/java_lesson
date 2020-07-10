a79c02bf89bda8760e14dbe6e0edc5ff7ab0b0ea
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-07-10 17:28:50 +0900

    [finish]java_base_chapter13

---
+++ practice/MobilePhone.java
@@ -0,0 +1,19 @@
+package practice;
+
+import extend.phone.Phone;
+
+public class MobilePhone implements Phone {
+    private String number;
+
+    public MobilePhone(String number) {
+        this.number = number;
+    }
+
+    public void takePicture() {
+        System.out.println("写真をとります。");
+    }
+
+    public void call(String number) {
+        System.out.println(this.number + " から " + number + " に電話をかけます。");
+    }
+}
