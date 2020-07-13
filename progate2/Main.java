a182ef31d96f03e523f1c6c146f940ef7235bbbd
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-07-13 15:12:13 +0900

    [update]progate_java_dojo2_5/10

--- progate2/Main.java
+++ progate2/Main.java
@@ -2,9 +2,13 @@ package progate2;
 
 public class Main {
 	public static void main(String[] args) {
-		Bicycle bicycle = new Bicycle("ビアンキ");
+		Bicycle bicycle = new Bicycle("ビアンキ","緑");
 		System.out.println("【自転車の情報】");
-		System.out.println("名前：" + bicycle.getName());
+
+		bicycle.printData();
+
+		System.out.println("-----------------");
+		bicycle.run(10);
 	}
 
 }
